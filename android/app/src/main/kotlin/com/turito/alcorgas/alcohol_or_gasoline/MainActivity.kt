8c1ecb2905ff16de161b963e4ece899d01fad0a8
Artur Barbosa <89175768+fxbrartur@users.noreply.github.com>
2022-11-01 13:09:32 -0300

    deploying

--- android/app/src/main/kotlin/com/turito/alcorgas/alcohol_or_gasoline/MainActivity.kt
+++ android/app/src/main/kotlin/com/turito/alcorgas/alcohol_or_gasoline/MainActivity.kt
@@ -3,4 +3,35 @@ package com.turito.alcorgas.alcohol_or_gasoline
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    public HyBid.initialize("APP_TOKEN", this);
+
+    private val mInterstitial: HyBidInterstitialAd? = null
+
 }
+
+private fun loadInterstitial() {
+    mInterstitial = HyBidInterstitialAd(this, "ZONE_ID", object : Listener() {
+        @Override
+        fun onInterstitialLoaded() {
+        }
+
+        @Override
+        fun onInterstitialLoadFailed(error: Throwable?) {
+        }
+
+        @Override
+        fun onInterstitialImpression() {
+        }
+
+        @Override
+        fun onInterstitialDismissed() {
+        }
+
+        @Override
+        fun onInterstitialClick() {
+        }
+    })
+    mInterstitial.load()
+}
+
